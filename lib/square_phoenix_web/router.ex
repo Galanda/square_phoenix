d360d3d53d3807d09547efc1e719e21e11a2bd92
MrJohns1 <lykketrolle03@gmail.com>
2018-05-16 11:19:40 +0200

    Added documentation for radiobuttons

--- lib/square_phoenix_web/router.ex
+++ lib/square_phoenix_web/router.ex
@@ -17,6 +17,7 @@ defmodule SquarePhoenixWeb.Router do
     pipe_through :browser # Use the default browser stack
 
     get "/", PageController, :index
+	get "/radiobuttons", RadiobuttonsController, :index
   end
 
   # Other scopes may use custom stacks.
