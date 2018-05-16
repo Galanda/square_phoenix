dbd1111339023995dfde1d34eeb7c36304956f3c
Antice <ken.r.e.madsen@gmail.comgmail.com>
2018-05-16 10:24:52 +0200

    Added Chartist
    Chartist documentation and dependencies added.

--- lib/square_phoenix_web/router.ex
+++ lib/square_phoenix_web/router.ex
@@ -17,6 +17,7 @@ defmodule SquarePhoenixWeb.Router do
     pipe_through :browser # Use the default browser stack
 
     get "/", PageController, :index
+	get "/chartist", ChartistController, :index
   end
 
   # Other scopes may use custom stacks.
